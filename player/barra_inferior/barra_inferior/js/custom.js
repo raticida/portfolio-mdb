6adfc227ebfacab7f4dcb2b5e0d978ea6e502d19
Maicon <raticida@gmail.com>
2019-09-09 17:42:46 -0300

    implantação do player

---
+++ player/barra_inferior/barra_inferior/js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	
+	var qrcode = new QRCode("qrcode", {
+		width: 90,
+		height: 90
+	});		
+
+	var link;
+	
+	function getFeedExame() {
+	
+		$.getJSON('http://localhost/exame/index.php', function($data) {
+	
+			link = $data.link;			
+
+			$("#titulo").html($data.title);
+			$("#descricao").html($data.description);
+			
+			$('#loading').hide();
+				
+			makeCode();
+			
+			setTimeout(getFeedExame, 15000);
+			
+		}).done(function() {
+		
+			makeCode();
+			$('#loading').hide();
+		
+		});	
+		
+	};
+
+	function makeCode () {
+	
+		qrcode.makeCode(link);
+		
+	}
+
+	getFeedExame();	
+	
+});
---
+++ player/barra_lateral/barra_lateral/js/custom.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+
+	function getClimaTempo() {
+	
+		$.getJSON('http://canaltvon.com/lastro/clima_tempo.php', function($data) {
+						
+			$("#cidade").html($data.cidade);
+			$("#temperatura").html($data.temperatura);
+			$("#descricao").html($data.descricao);
+			$("#sensacao").html($data.sensacao);
+			$("#umidade").html($data.umidade);
+			$("#pressao").html($data.pressao);
+			$("#vento").html($data.vento);
+			$("#horario").html('Atualizado às: ' + $data.hora);
+			$('#icone').attr('src','barra_lateral/' + $data.icone);
+							
+			setTimeout(getClimaTempo, 900000);
+			
+		}).done(function() {
+		
+			getFinance();
+		
+		});
+		
+	};
+
+	function getFinance() {
+
+		$.getJSON('http://canaltvon.com/lastro/finance.php', function($data) {
+
+			$("#valor").html($data.valor);
+			$("#atualizacao").html($data.data);
+
+			if ($data.tipoVariacao == 'positivo') {
+			
+				$("#indice").html('<span style="color: #2FA605;">' + $data.indice + '</span>');
+				$('#iconeVariacao').attr('src','barra_lateral/icones/arrow_up.png');
+				
+			} else {
+			
+				$("#indice").html('<span style="color: #DC1313;">' + $data.indice + '</span>');
+				$('#iconeVariacao').attr('src','barra_lateral/icones/arrow_down.png');
+				
+			}
+			
+			setTimeout(getFinance, 10000);
+			
+		}).done(function() {
+		
+              var owl = $('.owl-carousel');
+			  
+              owl.owlCarousel({
+                items: 1,
+                loop: true,
+                autoplay: true,
+                autoplayTimeout: 10000,
+				dots: false,
+				mouseDrag: false,
+				touchDrag: false
+              });
+			 
+			 $('#loading').hide();
+			 
+		});
+	};
+	
+	function mostrarHora() {
+	
+	  $("#hora").html(moment().format('LTS'));
+	  
+	  setTimeout(mostrarHora, 1000);
+	  
+	}
+	
+	$("#data").html(moment().format('dddd') + ', '+ moment().format('LL'));
+	
+	getClimaTempo();
+	
+	mostrarHora();
+
+
+	
+});
